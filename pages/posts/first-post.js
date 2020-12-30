648f21264e4958efda5a682092ad5eb6765fe977
KojiroSchatten <banbirou@icloud.com>
2020-12-30 13:24:56 +0900

    done: css styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,20 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      {/* next/headからHeadを使用すると、そのままheadタグに対応する */}
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to Home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
