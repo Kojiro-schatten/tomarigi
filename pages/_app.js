d0554f2cc49dc15d192dd3cbc73f02f6ef01c4fe
KojiroSchatten <banbirou@icloud.com>
2020-12-31 12:49:19 +0900

    done: link, and conditional branch both home and first-post page

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+// Important: You need to restart the development server when you add pages/_app.js. 
+// Press Ctrl + c to stop the server and run:
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
